a59026abffa1da3c4f4313fa6e716d74eaafa3c0
MaxenceLLogal <maxence.l@logal.be>
2022-01-27 08:47:43 +0100

    first commit

---
+++ AppTestAPK/AppTestAPK/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppTestAPK
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+#if B2
+            label1.Text = "B2";
+#elif VDS 
+            label1.Text = "VDS";
+#endif
+        }
+    }
+}
